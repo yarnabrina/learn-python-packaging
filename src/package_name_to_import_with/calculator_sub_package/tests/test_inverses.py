4f33cb80751ed36784257e0f594b590d85430508
Anirban Ray <39331844+yarnabrina@users.noreply.github.com>
2023-03-04 20:00:52 +0530

    simplify unit tests for inverses

--- src/package_name_to_import_with/calculator_sub_package/tests/test_inverses.py
+++ src/package_name_to_import_with/calculator_sub_package/tests/test_inverses.py
@@ -1,38 +1,39 @@
 """Define unit tests for arithmetic operations."""
+import math
+
 import pytest
 
 from package_name_to_import_with import calculator_sub_package
 
-ADDITIVE_INVERSE_ELEMENT = 0
-MULTIPLICATIVE_INVERSE_ELEMENT = 1
 
-
-def test_successful_additive_inverse(first_number: float) -> None:
+def test_successful_additive_inverse(first_number: float, expected_negative: float) -> None:
     """Check additive inverse of a real number.
 
     Parameters
     ----------
     first_number : float
         value of input number
+    expected_negative : float
+        value of expected negative
     """
     result = calculator_sub_package.get_negative(first_number)
-
-    additive_inverse_check = calculator_sub_package.add_numbers(result, first_number)
-    assert additive_inverse_check == ADDITIVE_INVERSE_ELEMENT  # nosec B101
+    assert math.isclose(result, expected_negative)  # nosec B101
 
 
-def test_successful_multiplicative_inverse(second_number: float) -> None:
+def test_successful_multiplicative_inverse(
+    second_number: float, expected_reciprocal: float
+) -> None:
     """Check multiplicative inverse of a real number.
 
     Parameters
     ----------
     second_number : float
         value of input number
+    expected_reciprocal : float
+        value of expected reciprocal
     """
     result = calculator_sub_package.get_reciprocal(second_number)
-
-    multiplicative_inverse_check = calculator_sub_package.multiply_numbers(second_number, result)
-    assert multiplicative_inverse_check == MULTIPLICATIVE_INVERSE_ELEMENT  # nosec B101
+    assert math.isclose(result, expected_reciprocal)  # nosec B101
 
 
 def test_multiplicative_inverse_failure() -> None:
