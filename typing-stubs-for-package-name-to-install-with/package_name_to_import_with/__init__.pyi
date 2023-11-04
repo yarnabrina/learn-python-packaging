43ac14e8051d1342eaf5816bca78872202d77fa2
Anirban Ray <39331844+yarnabrina@users.noreply.github.com>
2023-11-04 16:25:40 +0530

    formatted stub files with isort and black

--- typing-stubs-for-package-name-to-install-with/package_name_to_import_with/__init__.pyi
+++ typing-stubs-for-package-name-to-install-with/package_name_to_import_with/__init__.pyi
@@ -1,5 +1,3 @@
-from .calculator_sub_package import calculate_results as calculate_results
-from .garbage_collection_module import (
-    define_garbage_collection_decorator as define_garbage_collection_decorator,
-)
-from .simplify import solve_simplification as solve_simplification
+from .calculator_sub_package import calculate_results
+from .garbage_collection_module import define_garbage_collection_decorator
+from .simplify import solve_simplification
--- typing-stubs-for-package-name-to-install-with/package_name_to_import_with/simplify.pyi
+++ typing-stubs-for-package-name-to-install-with/package_name_to_import_with/simplify.pyi
@@ -6,11 +6,9 @@ import pydantic
 
 from .calculator_sub_package import ArithmeticOperator
 
-
 class Parentheses(str, enum.Enum):
     LEFT: str
     RIGHT: str
-
 
 class TokenType(str, enum.Enum):
     POSITIVE_NUMBER: str
@@ -18,14 +16,13 @@ class TokenType(str, enum.Enum):
     OPERATOR: str
     PARENTHESIS: str
 
-
 OPERATION_PRECEDENCES: dict[ArithmeticOperator | Parentheses, int]
 
 def clean_and_tokenise_expression(
-    raw_expression: str
+    raw_expression: str,
 ) -> pydantic.InstanceOf[collections.abc.Iterator[re.Match[str]]]: ...
 def parse_infix_expression(
-    infix_expression_tokens: pydantic.InstanceOf[collections.abc.Iterator[re.Match[str]]]
+    infix_expression_tokens: pydantic.InstanceOf[collections.abc.Iterator[re.Match[str]]],
 ) -> list[ArithmeticOperator | float]: ...
 def evaluate_postfix_expression(postfix_expression: list[ArithmeticOperator | float]) -> float: ...
 def solve_simplification(expression: str) -> float: ...
